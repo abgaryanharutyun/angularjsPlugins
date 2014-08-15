a86377c3d3ee8b0ca39b4ea8e7cccbd1445c16ad
Harutyun Abgaryan <harutyunabgaryan@gmail.com>
2014-08-15 23:10:12 +0400

    my firs commit hideOnBlur.js

---
+++ hideOnBlur.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @author Harutyun Abgaryan harutyunAbgaryan@gmail.com
+ *
+ * @version 0.1 v
+ * Directive hideOnBlur for toggle open and Close
+ *
+ * @example
+ * <script src="../directives/hideOnBlur.js"></script>
+ * for clicked item set  class
+ * <div class="clickedClass">
+ *   <div hide-on-blure="clickedClass" 
+ */
+angular.module('App')
+  .directive('hideOnBlur', function () {
+    return {
+      restrict: 'ECMA',
+      link: function (scope, element, attrs) {
+        angular.element(document).ready(function () {
+          var clicked = true;
+          angular.element(document).click(function (e) {
+            angular.element(element).removeClass('hide');
+            angular.element(element).hide();
+            if (jQuery(e.target).hasClass(attrs.hideOnBlur)) {
+              if (clicked) {
+                angular.element(element).show();
+                clicked=false;
+              }else{
+                clicked=true;
+              }
+            }
+            if (!jQuery(e.target).hasClass(attrs.hideOnBlur)){
+              clicked=true;
+            }
+            if (jQuery(element).find(e.target).length){
+              angular.element(element).show();
+              clicked=false;
+            }
+          })
+        });
+      }
+    }
+  });
+
